c96d05b47315350acc59780fe80ad0ac6075f92c
Youngmin Koo <youngminz.kr@gmail.com>
2022-03-23 04:11:53 +0900

    feat: Implement basic functionality

--- android/src/main/kotlin/kr/youngminz/galaxy_store_in_app_review/GalaxyStoreInAppReviewPlugin.kt
+++ android/src/main/kotlin/kr/youngminz/galaxy_store_in_app_review/GalaxyStoreInAppReviewPlugin.kt
@@ -1,35 +1,155 @@
 package kr.youngminz.galaxy_store_in_app_review
 
-import androidx.annotation.NonNull
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.util.Log
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
-/** GalaxyStoreInAppReviewPlugin */
-class GalaxyStoreInAppReviewPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class GalaxyStoreInAppReviewPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+    private var channel: MethodChannel? = null
+    private var activity: Activity? = null
+    private var applicationContext: Context? = null
+    private var deeplinkUri: String? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "galaxy_store_in_app_review")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    companion object {
+        const val CHANNEL = "galaxy_store_in_app_review"
+        const val TAG = "GalaxyStoreInAppReview"
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, CHANNEL)
+        channel!!.setMethodCallHandler(this)
+        applicationContext = flutterPluginBinding.applicationContext
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel!!.setMethodCallHandler(null)
+        applicationContext = null
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        when (call.method) {
+            "isAvailable" -> {
+                mainScope.launch {
+                    if (!isGalaxyStoreReviewAPISupported()) {
+                        Log.d(TAG, "isAvailable: isGalaxyStoreReviewAPISupported() -> false")
+                        result.success(false)
+                        return@launch
+                    }
+                    if (!canWriteReview()) {
+                        Log.d(TAG, "isAvailable: canWriteReview() -> false")
+                        result.success(false)
+                        return@launch
+                    }
+                    Log.d(TAG, "isAvailable: true")
+                    result.success(true)
+                }
+            }
+            "requestReview" -> {
+                requestReview()
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    // Original source code : https://seller.samsungapps.com/notice/getNoticeDetail.as?csNoticeID=0000005510
+
+    private fun isGalaxyStoreReviewAPISupported(): Boolean {
+        /** To request the user to write a review,
+         * it is necessary to check whether the Galaxy Store of the device supports the Review API.
+         * Rating API is supported from Galaxy Store Client 4.5.22.7 or later.
+         * */
+
+        val applicationInfo = applicationContext!!.packageManager.getApplicationInfo("com.sec.android.app.samsungapps", PackageManager.GET_META_DATA)
+        val inAppReviewVersion = applicationInfo.metaData.getInt("com.sec.android.app.samsungapps.review.inappReview", 0)
+        return inAppReviewVersion > 0
+    }
+
+    private suspend fun canWriteReview(): Boolean {
+        /** After checking the Galaxy Store version,
+         * it requires to check whether the user can write a review.
+         * Returns true if all of the conditions below are satisfied.
+         *
+         * - Logged into Samsung Account
+         * - App downloaded or updated from Galaxy Store
+         * - No review history within the last year
+         */
+
+        // 1. Check your review authority by Galaxy Store package
+        val intent = Intent("com.sec.android.app.samsungapps.REQUEST_INAPP_REVIEW_AUTHORITY")
+        intent.setPackage("com.sec.android.app.samsungapps")
+        intent.putExtra("callerPackage", applicationContext!!.packageName)
+        applicationContext!!.sendBroadcast(intent)
+
+        // 2. Get result of authority checking from Galaxy Store package
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("com.sec.android.app.samsungapps.RESPONSE_INAPP_REVIEW_AUTHORITY")
+
+        return suspendCoroutine { continuation ->
+            val broadcastReceiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    applicationContext!!.unregisterReceiver(this)
+
+                    // If true, you have authority to write review
+                    val hasAuthority = intent!!.getBooleanExtra("hasAuthority", false)
+
+                    // By using deeplinkUrlForReview, you can open review activity of Galaxy Store
+                    deeplinkUri = intent.getStringExtra("deeplinkUri")
+
+                    continuation.resume(hasAuthority)
+                }
+            }
+
+            applicationContext!!.registerReceiver(broadcastReceiver, intentFilter)
+        }
+    }
+
+    private fun requestReview() {
+        /** If the user's status is confirmed through the above process,
+         * a review writing screen is provided to the user. */
+
+        if (deeplinkUri == null) {
+            Log.e(TAG, "Before calling the requestReview, it is required to call the isAvailable and check if true is returned.")
+            return
+        }
+
+        val intent = Intent()
+        intent.data = Uri.parse(deeplinkUri)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+        activity!!.startActivity(intent)
+    }
 }
--- lib/galaxy_store_in_app_review.dart
+++ lib/galaxy_store_in_app_review.dart
@@ -1,13 +1,33 @@
-
 import 'dart:async';
+import 'dart:io' show Platform;
 
 import 'package:flutter/services.dart';
 
 class GalaxyStoreInAppReview {
-  static const MethodChannel _channel = MethodChannel('galaxy_store_in_app_review');
+  static const MethodChannel _channel =
+      MethodChannel('galaxy_store_in_app_review');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// Checks if the device is able to show a Galaxy Store review dialog.
+  static Future<bool> isAvailable(
+      {Duration timeout = const Duration(seconds: 10)}) async {
+    // Galaxy Store is available only on Android devices.
+    if (!Platform.isAndroid) return false;
+
+    // A timeout may occur due to network issues.
+    // In this case, a TimeoutException is thrown.
+    try {
+      return await _channel.invokeMethod("isAvailable").timeout(timeout);
+    } on TimeoutException {
+      return false;
+    }
+  }
+
+  /// Attempts to show the review dialog. It's recommended to first check if
+  /// the device supports this feature via [isAvailable].
+  static Future<void> requestReview() async {
+    // Galaxy Store is available only on Android devices.
+    if (!Platform.isAndroid) return;
+
+    await _channel.invokeMethod("requestReview");
   }
 }
