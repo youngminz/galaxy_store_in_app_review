bd35c164d120c34cff80496f7c415e0f75cf3db7
Youngmin Koo <youngminz.kr@gmail.com>
2022-03-24 03:23:42 +0900

    feat: Add demo app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:galaxy_store_in_app_review/galaxy_store_in_app_review.dart';
 
 void main() {
@@ -16,34 +13,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  final _packageNameController = TextEditingController();
+  bool _isAvailable = false;
+  String _debugMessage = "";
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await GalaxyStoreInAppReview.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -51,10 +27,67 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('GalaxyStore Inapp Review Sample'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _packageNameController,
+                      decoration: const InputDecoration(
+                        labelText: "Input your package name",
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      var targetPackage = _packageNameController.text.isEmpty
+                          ? null
+                          : _packageNameController.text;
+                      await GalaxyStoreInAppReview.openStoreListing(
+                          targetPackage: targetPackage);
+                    },
+                    child: const Text("OPEN STORE"),
+                  )
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  var targetPackage = _packageNameController.text.isEmpty
+                      ? null
+                      : _packageNameController.text;
+
+                  setState(() {
+                    _debugMessage =
+                        "Checking availability - ${targetPackage ?? "(Current app)"}";
+                  });
+
+                  var isAvailable = await GalaxyStoreInAppReview.isAvailable(
+                      targetPackage: targetPackage);
+
+                  setState(() {
+                    _debugMessage =
+                        "${targetPackage ?? "(Current app)"} is ${isAvailable ? "" : "not "}available for review.";
+                    _isAvailable = isAvailable;
+                  });
+                },
+                child: const Text("CHECK REVIEW AUTHORITY"),
+              ),
+              ElevatedButton(
+                onPressed: _isAvailable
+                    ? () async {
+                        await GalaxyStoreInAppReview.requestReview();
+                      }
+                    : null,
+                child: const Text("OPEN GALAXYSTORE REVIEW POPUP"),
+              ),
+              Text(_debugMessage)
+            ],
+          ),
         ),
       ),
     );
